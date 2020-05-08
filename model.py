5754960a9db102a7942bb01b89e55837ba426720
Mikel Broström <mikelbrostrom@outlook.com>
2020-05-08 17:22:15 +0200

    new model added, wider and deeper

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 import torch
-from torch_geometric.nn import TopKPooling, SAGEConv
+from torch_geometric.nn import TopKPooling, SAGEConv, GraphConv
 from torch_geometric.nn import global_mean_pool as gap, global_max_pool as gmp
 import torch.nn.functional as F
 
@@ -53,5 +53,66 @@ class Net(torch.nn.Module):
         x = F.dropout(x, p=0.5, training=self.training)
 
         x = torch.sigmoid(self.lin3(x)).squeeze(1)
+
+        return x
+
+
+class Net2(torch.nn.Module):
+    def __init__(self, different_ids=52737, embed_dim=512):
+        super(Net2, self).__init__()
+
+        self.item_embedding = torch.nn.Embedding(num_embeddings=different_ids, embedding_dim=embed_dim)
+
+        self.conv1 = SAGEConv(embed_dim, 512)
+        self.pool1 = TopKPooling(512, ratio=0.8)
+        self.conv2 = SAGEConv(512, 512)
+        self.pool2 = TopKPooling(512, ratio=0.8)
+        self.conv3 = SAGEConv(512, 512)
+        self.pool3 = TopKPooling(512, ratio=0.8)
+        self.lin1 = torch.nn.Linear(1024, 512)
+        self.lin2 = torch.nn.Linear(512, 256)
+        self.lin3 = torch.nn.Linear(256, 128)
+        self.lin4 = torch.nn.Linear(128, 64)
+        self.lin5 = torch.nn.Linear(64, 1)
+        self.bn1 = torch.nn.BatchNorm1d(256)
+        self.bn2 = torch.nn.BatchNorm1d(128)
+        self.bn3 = torch.nn.BatchNorm1d(64)
+        self.act = torch.nn.ReLU()
+
+
+    def forward(self, data):
+        x, edge_index, batch = data.x, data.edge_index, data.batch
+        x = self.item_embedding(x)
+        x = x.squeeze(1)
+
+        x = F.relu(self.conv1(x, edge_index))
+
+        x, edge_index, _, batch, _, _ = self.pool1(x, edge_index, None, batch)
+
+        x1 = torch.cat([gmp(x, batch), gap(x, batch)], dim=1)
+
+        x = F.relu(self.conv2(x, edge_index))
+
+        x, edge_index, _, batch, _, _ = self.pool2(x, edge_index, None, batch)
+        x2 = torch.cat([gmp(x, batch), gap(x, batch)], dim=1)
+
+        x = F.relu(self.conv3(x, edge_index))
+
+        x, edge_index, _, batch, _, _ = self.pool3(x, edge_index, None, batch)
+        x3 = torch.cat([gmp(x, batch), gap(x, batch)], dim=1)
+
+        x = x1 + x2 + x3
+
+        x = self.lin1(x)
+        x = self.act(x)
+        x = self.lin2(x)
+        x = self.act(x)
+        x = self.lin3(x)
+        x = self.act(x)
+        x = self.lin4(x)
+        x = self.act(x)
+        x = F.dropout(x, p=0.5, training=self.training)
+
+        x = torch.sigmoid(self.lin5(x)).squeeze(1)
 
         return x
